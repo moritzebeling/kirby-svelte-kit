12bd4a0dad32fedd4cbedd1e87225da24a833f46
moritzebeling <git@moritzebeling.com>
2021-05-21 18:39:57 +0200

    switch from sapper to svelte-kit

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,9 @@
-const sveltePreprocess = require('svelte-preprocess');
-module.exports = {
-  preprocess: sveltePreprocess({
-    defaults: {
-      style: 'scss'
-    },
-    scss: {
-      includePaths: ['src/styles'],
-      prependData: `@import './mixins';`
-    },
-    postcss: {
-      plugins: [require('autoprefixer')],
-    },
-  }),
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte'
+	}
 };
+
+export default config;
